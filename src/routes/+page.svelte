de683efcdc6c607dc9cbe5e678bec4a93e936e17
Hsienz <55347238+Hsienz@users.noreply.github.com>
2023-02-14 19:38:10 +0800

    improve background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 <div
 	class="relative bg-Light_Grayish_Cyan_Background  pt-48 px-6 pb-16 min-h-screen min-w-[375px] md:px-16"
 >
-	<div class="absolute top-0 left-0 w-full h-[180px] bg-Desaturated_Dark_Cyan bg-cover bg-[url('/images/bg-header-mobile.svg')] md:bg-[url('/images/bg-header-desktop.svg')] bg-no-repeat"></div>
+	<div class="absolute top-0 left-0 w-full h-[120px] bg-Desaturated_Dark_Cyan bg-cover bg-[url('/images/bg-header-mobile.svg')] md:bg-[url('/images/bg-header-desktop.svg')] bg-no-repeat"></div>
     <Filter />    
 	<Jobs />
 </div>
